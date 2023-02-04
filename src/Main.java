7162f19a1a9aa6e5d8c2336843195b9c2b9b989f
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-02-04 20:36:00 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Author pushkin = new Author("Александр", "Пушкин");
+       Author noses = new Author("Николай", "Носов");
+
+       Book dreamers = new Book("Фантазеры",noses,1940);
+       Book eugeneOnegin = new Book("Евгений Онегин",pushkin,1822);
+
+       dreamers.printInfoBook();
+       eugeneOnegin.printInfoBook();
+
+       eugeneOnegin.setPublicationYear(1900);
+
+       eugeneOnegin.printInfoBook();
     }
 }
