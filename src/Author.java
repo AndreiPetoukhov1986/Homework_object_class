fb1bd6a6c0baa106663a36e10fb2a4d77c642566
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-02-07 23:33:21 +0300

    Second Commit

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -12,4 +14,29 @@ public class Author {
     public String getSurname(){
         return this.surname;
     }
-}
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+    @Override
+    public boolean equals (Object a){
+        if (this==a) {
+            return true;
+        }
+        if (a==null || getClass()!=a.getClass()) {
+            return false;
+        }
+        Author author = (Author) a;
+        return name.equals(author.name)&&surname.equals(author.surname);
+        }
+        
+        @Override
+    public int hashCode(){
+        return Objects.hash(name, surname);
+        }
+        
+    }
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ public class Main {
     public static void main(String[] args) {
        Author pushkin = new Author("Александр", "Пушкин");
        Author noses = new Author("Николай", "Носов");
+       Author noses2 = new Author("Николай", "Носов");
+
+        System.out.println(noses.equals(noses2));
 
        Book dreamers = new Book("Фантазеры",noses,1940);
        Book eugeneOnegin = new Book("Евгений Онегин",pushkin,1822);
 
-       dreamers.printInfoBook();
-       eugeneOnegin.printInfoBook();
+        System.out.println(dreamers.equals(eugeneOnegin));
+
+
+       System.out.println(dreamers);
+       System.out.println(eugeneOnegin);
 
        eugeneOnegin.setPublicationYear(1900);
 
-       eugeneOnegin.printInfoBook();
+       System.out.println(eugeneOnegin);
+
+        System.out.println(eugeneOnegin.hashCode());
+        System.out.println(noses2.hashCode());
+        System.out.println(noses.hashCode());
+
+
+
     }
 }
